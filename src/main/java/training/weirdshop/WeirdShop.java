139e297252aad9b509b6bc209dd00a209900f80f
ConBle <conor.bleakley@softwire.com>
2025-01-15 16:36:27 +0000

    Separate Backstage Pass from regular items

--- src/main/java/training/weirdshop/WeirdShop.java
+++ src/main/java/training/weirdshop/WeirdShop.java
@@ -11,38 +11,30 @@ class WeirdShop {
         for (Item item : items) {
             if (item.name.equals("Gold Coin")) continue;
 
+            item.sellIn--;
+
             if (item.name.equals("Aged Brie")) {
-                item.sellIn--;
                 item.quality += (item.sellIn < 0) ? 2 : 1;
                 if (item.quality > 50) item.quality = 50;
                 continue;
             }
 
-            if (!item.name.equals("Backstage Pass")) {
-                if (item.quality > 0) {
-                    item.quality--;
-                }
-            } else if (item.quality < 50) {
-                if (item.sellIn < 7) {
-                    item.quality = (item.quality > 47) ? 50 : item.quality + 3;
-                } else if (item.sellIn < 12) {
-                    item.quality = (item.quality > 48) ? 50 : item.quality + 2;
+            if (item.name.equals("Backstage Pass")) {
+                if (item.sellIn < 6) {
+                    item.quality += 3;
+                } else if (item.sellIn < 11) {
+                    item.quality += 2;
                 } else {
-                    item.quality++;
+                    item.quality += 1;
                 }
+
+                if (item.quality > 50) item.quality = 50;
+                if (item.sellIn < 0) item.quality = 0;
+                continue;
             }
 
-            item.sellIn--;
-
-            if (item.sellIn < 0) {
-                if (!item.name.equals("Backstage Pass")) {
-                    if (item.quality > 0) {
-                        item.quality--;
-                    }
-                } else {
-                    item.quality = 0;
-                }
-            }
+            item.quality -= (item.sellIn < 0) ? 2 : 1;
+            if (item.quality < 0) item.quality = 0;
         }
     }
 }

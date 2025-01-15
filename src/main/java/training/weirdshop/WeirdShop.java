928655829c758462398486453fe8be3cd0122fd8
ConBle <conor.bleakley@softwire.com>
2025-01-15 16:13:47 +0000

    Simplify Aged Brie handling

--- src/main/java/training/weirdshop/WeirdShop.java
+++ src/main/java/training/weirdshop/WeirdShop.java
@@ -9,43 +9,38 @@ class WeirdShop {
 
     void updateQuality() {
         for (Item item : items) {
-            if (item.name.equals("Gold Coin")) {
+            if (item.name.equals("Gold Coin")) continue;
+
+            if (item.name.equals("Aged Brie")) {
+                item.sellIn--;
+                item.quality += (item.sellIn < 0) ? 2 : 1;
+                if (item.quality > 50) item.quality = 50;
                 continue;
             }
 
-            if (!item.name.equals("Aged Brie") && !item.name.equals("Backstage Pass")) {
+            if (!item.name.equals("Backstage Pass")) {
                 if (item.quality > 0) {
                     item.quality--;
                 }
             } else if (item.quality < 50) {
-                if (!item.name.equals("Backstage Pass")) {
-                    item.quality++;
+                if (item.sellIn < 7) {
+                    item.quality = (item.quality > 47) ? 50 : item.quality + 3;
+                } else if (item.sellIn < 12) {
+                    item.quality = (item.quality > 48) ? 50 : item.quality + 2;
                 } else {
-                    if (item.sellIn < 7) {
-                        item.quality = (item.quality > 47) ? 50 : item.quality + 3;
-                    } else if (item.sellIn < 12) {
-                        item.quality = (item.quality > 48) ? 50 : item.quality + 2;
-                    } else {
-                        item.quality++;
-                    }
+                    item.quality++;
                 }
             }
 
             item.sellIn--;
 
             if (item.sellIn < 0) {
-                if (!item.name.equals("Aged Brie")) {
-                    if (!item.name.equals("Backstage Pass")) {
-                        if (item.quality > 0) {
-                            item.quality--;
-                        }
-                    } else {
-                        item.quality = 0;
+                if (!item.name.equals("Backstage Pass")) {
+                    if (item.quality > 0) {
+                        item.quality--;
                     }
                 } else {
-                    if (item.quality < 50) {
-                        item.quality++;
-                    }
+                    item.quality = 0;
                 }
             }
         }
